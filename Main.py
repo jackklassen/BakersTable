0b361fd3a5541614b74fad3b5725eedb980e1ad5
nul <jackaklassen.jak@gmail.com>
2019-05-27 15:27:34 -0700

    added CRD from CRUD methods to recipe list

--- Main.py
+++ Main.py
@@ -1,7 +1,17 @@
 import recipeobject as ro
+#tester
 
 recipe = ro.recipe()
+
+test = dict()
+name ="levain"
+test['Flour'] = 100
+test['Water'] = 100
+
+recipe.addtorecipe(name,test)
+
 
 recipe.addtorecipe("test",5)
 
 print(recipe.readfromrecipe("test"))
+print(recipe.readfromrecipe(name))
--- RecipeList.py
+++ RecipeList.py
@@ -1,5 +1,23 @@
+import recipeobject as recipeobject
 class recipelist(object):
 
     def __init__(self):
-        self.RecipeList = list()
-    
+        self.List = list()
+        #create
+    def addrecipe(self,recipe):
+        self.List.append(recipe)
+
+        #read
+    def getrecipe(self,recipe):
+        for x in self.List:
+            if recipe.equals(x):
+                return x
+            else:
+                return 0 
+
+        #update
+                
+
+        #delete
+        def deleterecipe(self,recipe):
+            self.list.remove(recipe)
